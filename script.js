d315ef1bc1744e848c4c7609fef7b6232cbdce8b
walauceh <145186203+walauceh@users.noreply.github.com>
2023-09-29 11:58:08 +0000

    redo

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript for Page 1
+
+
+
+// Add code to fetch and display the product list here
+
+// Function to navigate to Page 2
+function navigateToPage2(productId) {
+    // Add code to load Page 2 with product details
+}
+
+// Function to navigate to Page 3
+function navigateToPage3() {
+    // Add code to load Page 3 for wallet operations
+}
